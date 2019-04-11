5973233e5254eea574b97f199c95cf98af78e843
janis <jaanis.tima@gmail.com>
2019-04-11 11:33:57 +0300

    Initial Commit

---
+++ myCalculatorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myCalculatorApp
+//
+//  Created by egmars.janis.timma on 11/04/2019.
+//  Copyright © 2019 egmars.janis.timma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
