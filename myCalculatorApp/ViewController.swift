4b5b188f4a28141c5f295558758bb4def2b97453
Egmars Janis Timma <egmars.janis.timma@accenture.com>
2019-04-15 18:45:23 +0300

    Made changes with answer

--- myCalculatorApp/ViewController.swift
+++ myCalculatorApp/ViewController.swift
@@ -10,135 +10,113 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
-
-    var player: AVAudioPlayer = AVAudioPlayer()
     
-    @IBAction func Play(_ sender: Any){
-        player.play()
+    @IBOutlet var label: UILabel!
+    @IBOutlet var buttons: [UIButton]!
+    
+    @IBAction func play(_ sender: Any) {
+        player?.play()
     }
     
-    @IBOutlet var buttons: [UIButton]!
-
+    @IBAction func pressNum(sender: UIButton) {
+        if answer == "0" {
+            answer = String(sender.tag)
+        } else {
+            answer += String(sender.tag)
+        }
+        
+        printNumber()
+    }
+    
+    @IBAction func calculate() {
+        switch operand {
+        case .add:
+            answer = String(num1 + num2)
+        case .divide:
+            answer = String(num1 - num2)
+        case .multiply:
+            answer = String(num1 * num2)
+        case .subtract:
+            if num2 == 0 {
+                answer = "Divided by 0"
+            } else {
+                answer = String(num1 / num2)
+            }
+        }
+    }
+    
+    @IBAction func setOperand(sender: UIButton) {
+        operand = Operand(rawValue: sender.tag) ?? .add
+        saveNum1()
+        
+    }
+    
+    @IBAction func clearOperation() {
+        num1 = 0
+        num2 = 0
+        answer = "0"
+        
+        printNumber()
+    }
+    
     enum Operand: Int {
         case add = 10
         case subtract = 11
         case multiply = 12
         case divide = 13
     }
-    var theNumber = ""
-//    let plus = 10
-//    let minus = 11
-//    let multiply = 12
-//    let divide = 13
     
-    @IBAction func buttonPressed(sender: UIButton){
-    theNumber += "\(sender.tag)"
-    lblText.text = theNumber
-    }
+    var player: AVAudioPlayer?
     
-    @IBOutlet var lblText : UILabel!
-    
-    var num1 = 0.0
-    var num2 = 0.0
-    var operand = 0
-    var answer = 0.0
- 
-    @IBAction func calculate(sender : UIButton) {
-        num2 = Double(Int(theNumber)!)
-        
-        switch operand {
-        case Operand.add.rawValue:
-            answer = num1 + num2
-        case Operand.subtract.rawValue:
-            answer = num1 - num2
-        case Operand.multiply.rawValue:
-            answer = num1 * num2
-        default:
-            answer = num1 / num2
-        }
-//        if operand == plus {
-//            answer = Double(num1 + num2)
-//        }
-//        if operand == minus {
-//            answer = Double(num1 - num2)
-//        }
-//        if operand == multiply {
-//            answer = Double(num1 * num2)
-//        }
-//        if operand == divide {
-//
-//            if num2 == 0 {
-//                let alert = UIAlertController(title: "Error", message: "Cant divide by zero", preferredStyle: .alert)
-//                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-//                alert.addAction(cancelAction)
-//                present(alert, animated: true)
-//            } else {
-//            answer = Double(num1) / Double(num2)
-//        }
-//    }
-       clear()
-    }
-    
-    func clear(){
-        num1 = 0.0
-        num2 = answer
-        operand = Operand.add.rawValue
-        theNumber = String(answer)
-        printNumber()
-        answer = num2
-        theNumber = ""
-    }
-    
-    @IBAction func setOperand(sender : UIButton) {
-        if theNumber != "" {
-            operand = sender.tag
-            saveNum1()
-            theNumber = ""
+    var num1 = 0.0 {
+        didSet {
+            print("Did save number 1: \(num1)")
         }
     }
-    
-    @IBAction func clearOperation(_ sender: UIButton) {
-        theNumber = "0"
-        printNumber()
+    var num2 = 0.0 {
+        didSet {
+            print("Did save number 2: \(num2)")
+        }
     }
-
-    func saveNum1() {
-        num1 = Double(Int(theNumber)!)
-        theNumber = String(num1)
-        printNumber()
+    var operand: Operand = .add
+    var answer = "0" {
+        didSet {
+            print("Current answer: \(answer)")
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        do{
-            let audioPlayer = Bundle.main.path(forResource: "tap", ofType: "wav")
-            try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPlayer!) as URL)
-                }
-        catch{
-            //ERROR
+        
+        if let resourcePath = Bundle.main.path(forResource: "tap", ofType: "wav") {
+            player = try? AVAudioPlayer.init(contentsOf: URL(fileURLWithPath: resourcePath))
         }
         
-        for i in buttons {
-            i.imageView?.contentMode = .scaleAspectFit
+        for button in buttons {
+            button.imageView?.contentMode = .scaleAspectFit
         }
-        theNumber = ""
+        
         printNumber()
     }
     
-    func printNumber() {
-        let newNumber = theNumber.replacingOccurrences(of: ".0", with: "")
-//        let formatter = NumberFormatter()
-//        formatter.minimumIntegerDigits = 1
-//        formatter.minimumFractionDigits = 0
-//        formatter.maximumFractionDigits = 15
-//        let newNumber = formatter.string(for: theNumber)
-        lblText.text = newNumber
+    func saveNum1() {
+        if let number = Double(answer) {
+            num1 = number
+        }
+        
+        printNumber()
     }
     
-    @IBAction func pressNum(sender : UIButton) {
-//            theNumber = ""
-//            theNumber += String(sender.tag)
-            printNumber()
+    func saveNum2() {
+        if let number = Double(answer) {
+            num2 = number
+        }
+        
+        calculate()
+    }
+    
+    func printNumber() {
+        label.text = answer.replacingOccurrences(of: ".0", with: "")
     }
 }
 
